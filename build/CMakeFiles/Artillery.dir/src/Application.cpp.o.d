b389ce00ee525bb9c10d94a2c64e910bd514ea09
Gyorgy Brincken <brinckengyurika@gmail.com>
2026-07-29 02:39:36 +0200

    Fix CodeBlocks path and Ogre 2.3 compilation

--- build/CMakeFiles/Artillery.dir/src/Application.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Artillery.dir/src/Application.cpp.o: \
- /home/satch/Projects/Artillery/src/Application.cpp \
- /usr/include/stdc-predef.h \
- /home/satch/Projects/Artillery/src/Application.h
